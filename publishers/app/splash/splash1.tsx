f25f56326427066ab6390d647922f378420b35ee
mostcharming <mostcharming920@yahoo.com>
2026-01-20 01:16:51 +0100

    starting with splash pages

--- publishers/app/splash/splash1.tsx
+++ publishers/app/splash/splash1.tsx
@@ -1,19 +1,58 @@
-import React from "react";
-import { Text, View } from "react-native";
+import { useAssets } from "expo-asset";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import { SvgUri } from "react-native-svg";
 
 export default function Splash1() {
+  const { height } = useWindowDimensions();
+  const [assets] = useAssets([require("@/assets/images/carsl.svg")]);
+  const [carslUri, setCarslUri] = useState<string>("");
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#000000",
+      paddingTop: height * 0.1,
+    },
+    textSection: {
+      alignItems: "flex-start",
+      marginBottom: 0,
+    },
+    heading: {
+      fontSize: 35,
+      fontWeight: "bold",
+      color: "#FFFFFF",
+      fontFamily: "BankGothicBold",
+      lineHeight: 30,
+    },
+    rowContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: 10,
+    },
+    splashText: {
+      fontSize: 32,
+      fontWeight: "bold",
+      color: "#FFFFFF",
+    },
+  });
+
+  useEffect(() => {
+    if (assets && assets[0]) {
+      setCarslUri(assets[0].uri);
+    }
+  }, [assets]);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#000000",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 32, fontWeight: "bold", color: "#FFFFFF" }}>
-        Splash 1
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.textSection}>
+        <Text style={styles.heading}>GET DISCOVERED</Text>
+        <View style={styles.rowContainer}>
+          <Text style={styles.heading}>WITH</Text>
+          {carslUri && <SvgUri uri={carslUri} />}
+        </View>
+      </View>
+      <Text style={styles.splashText}>Splash 1</Text>
     </View>
   );
 }
