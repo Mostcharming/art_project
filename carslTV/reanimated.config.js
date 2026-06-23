021fd60c07e52227ec11108cc57c08d2d4481aaf
mostcharming <mostcharming920@yahoo.com>
2026-06-23 09:14:58 +0100

    Update project files

--- carslTV/reanimated.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // Disable strict mode warnings about reading/writing shared values during render
-    // This is a known issue with certain libraries that use reanimated internally
-    disableNativeInitialization: false,
-};
